20361fe49cd7617c202f34a2ed38a3ca0bfc1eae
mavil001 <marieville@hotmail.fr>
2023-02-09 21:20:48 -0500

    -Templates html de base (ceux visibles de l'accueil) -Routes fonctionnelles -Urls -Html/css conforme à maquette et liens fonctionnels

--- projet_web_app/crazymix/urls.py
+++ projet_web_app/crazymix/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('infos/',views.infos,name="infos")
+    path('reservation/new', views.reservation, name="reservation"),
+    path('sessions/', views.sessions, name="sessions"),
+    path('extraits_artistes/', views.extraits_artistes, name="extraits_artistes"),
+    path('connexion/', views.connexion, name="connexion"),
+    path('inscription/new', views.inscription, name="inscription"),
+    path('compte/', views.compte, name="compte"),
+    path('infos/', views.infos, name="infos")
 
 ]
--- projet_web_app/crazymix/views.py
+++ projet_web_app/crazymix/views.py
@@ -4,7 +4,25 @@ from django.shortcuts import render
 
 
 def index (request):
-    return render (request,'crazymix/index.html')
+    return render(request,'crazymix/index.html', {'title':'CrazyMix - Studio'})
+
+def reservation(request):
+    return render(request,'crazymix/reservation.html', {'title':"Réserver une session d'enregistrement"})
+
+def sessions(request):
+    return render(request,'crazymix/sessions.html', {'title':"Mes sessions d'enregistrement"})
+
+def extraits_artistes(request):
+    return render(request,'crazymix/extraits_artistes.html', {'title':'Exraits - Artistes'})
+
+def connexion(request):
+    return render(request,'crazymix/connexion.html', {'title':'Se connecter'})
+
+def inscription(request):
+    return render(request,'crazymix/inscription.html', {'title':"S'inscrire"})
+
+def compte(request):
+    return render(request,'crazymix/compte.html', {'title':'Mon compte'})
 
 def infos(request):
-    return render(request,'crazymix/infos.html')
+    return render(request,'crazymix/infos.html', {'title':'Informations sur le site du studio'})
